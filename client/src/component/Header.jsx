4f6b3368502740aceab95514a535cbfe7aa9254e
shrikant mishra <mishrashri2001@gmail.com>
2024-01-16 15:21:47 +0530

    Some modification

---
+++ client/src/component/Header.jsx
@@ -0,0 +1,26 @@
+import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold
+    
+    dark:text-white
+    "
+      >
+        <span
+          className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-400 
+      to-pink-500
+      "
+        >
+          SHRI
+        </span>
+        Blog
+      </Link>
+    </Navbar>
+  );
+};
+
+export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: [ "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  'node_modules/flowbite-react/lib/esm/**/*.js',
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+
+  ],
 }
+
