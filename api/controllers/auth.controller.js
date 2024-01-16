2d0efe966bbc853079d3cd3b6b7d306d4ff9cd81
shrikant mishra <mishrashri2001@gmail.com>
2024-01-16 19:01:55 +0530

    add a middlewere and push in production

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res,next) => {
   //  Console
   const { username, email, password } = req.body;
   if (
@@ -11,7 +12,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ mesage: "All field are required ..!" });
+       next(errorHandler(400,'All field are required..!'))
   }
   const hashedPassword=bcryptjs.hashSync(password,12)
   const newUser=new User ({username,email,password:hashedPassword});
@@ -19,7 +20,7 @@ export const signup = async (req, res) => {
 
     await newUser.save();
     res.json({message:"DATA INSERT"});
- }catch(err){
-      res.status(500).json({message:err.message});
+ }catch(error){
+    next(error);
  }
 };
