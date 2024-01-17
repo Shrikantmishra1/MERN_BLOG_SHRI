a5a760596bc1c6444f53e6f524d8c86f4ee53d78
shrikant mishra <mishrashri2001@gmail.com>
2024-01-17 12:57:16 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   //  Console
   const { username, email, password } = req.body;
   if (
@@ -12,15 +13,47 @@ export const signup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-       next(errorHandler(400,'All field are required..!'))
+    next(errorHandler(400, "All field are required..!"));
   }
-  const hashedPassword=bcryptjs.hashSync(password,12)
-  const newUser=new User ({username,email,password:hashedPassword});
- try{
-
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.json({message:"DATA INSERT"});
- }catch(error){
+    res.json({ message: "DATA INSERT" });
+  } catch (error) {
     next(error);
- }
+  }
+};
+/**Sign in process Code ): */
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All field are required..!"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid password"));
+    }
+
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+const {password:pass,...rest}=validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
