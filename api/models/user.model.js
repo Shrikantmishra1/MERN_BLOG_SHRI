859e6ec24093fef2ba34e510e964b1056e1e89b5
shrikant mishra <mishrashri2001@gmail.com>
2024-01-18 13:03:20 +0530

    Google Oauth functionality..

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema=new mongoose.Schema({
               type:String,
               required:true,
         },
+        profilePicture:{
+               type:String,
+               default:'https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg'
+        }
 },
 {timestamps:true}
 );
--- client/src/component/OAuth.jsx
+++ client/src/component/OAuth.jsx
@@ -1,9 +1,38 @@
 import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
-
+import {GoogleAuthProvider,signInWithPopup,getAuth} from 'firebase/auth';
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 const OAuth = () => {
-    const handelGoogleClick=()=>{
-           console.log("Hello");
+    const auth=getAuth(app);
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const handelGoogleClick= async()=>{
+           const provider=new GoogleAuthProvider();
+           provider.setCustomParameters({prompt:'select_account'});
+           try{
+            const resultsFromGoogle=await signInWithPopup(auth,provider);
+                const res=await fetch('/api/auth/google',{
+                      method:'POST',
+                      headers:{'content-type': 'application/json'},
+                      body:JSON.stringify({
+                           name:resultsFromGoogle.user.displayName,
+                           email:resultsFromGoogle.user.email,
+                           googlePhotoUrl:resultsFromGoogle.user.photoURL,
+
+                      }),
+                })
+               const data=await res.json();
+                if(res.ok){
+                     dispatch(signInSuccess(data)) ;
+                     navigate('/');
+                }
+
+           }catch(error){
+              console.log(error);
+           }
     }
   return (
     <Button 
@@ -12,7 +41,7 @@ const OAuth = () => {
     
     type="button" gradientDuoTone="pinkToOrange" outline>
       <AiFillGoogleCircle className="w-6 h-6 mr-2" />
-      Sign In With Google
+    continue In With Google
     </Button>
   );
 };
