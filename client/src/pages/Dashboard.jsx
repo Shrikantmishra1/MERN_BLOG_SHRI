61a023bf3a90816776fc98982251b5291a3197a8
shrikant mishra <mishrashri2001@gmail.com>
2024-01-18 15:53:44 +0530

    complete sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+
+import { useState,useEffect } from 'react';
+import {useLocation} from 'react-router-dom';
+import DashSidebar from '../component/DashSidebar';
+import DashProfile from '../component/DashProfile';
 
 const Dashboard = () => {
+  const location=useLocation();
+  const [tab,setTab]=useState('');
+  useEffect(() => {
+        const urlParms=new URLSearchParams(location.search);
+        const tabFromUrl=urlParms.get('tab');
+              if(tabFromUrl){
+                   setTab(tabFromUrl);
+              }
+  
+  }, [location.search])
+  
   return (
-    <div>Dashboard</div>
+    <div  className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+      {/* PRofile and ..... */}
+        {tab ==='profile' && <DashProfile/>}
+    </div>
   )
 }
 
