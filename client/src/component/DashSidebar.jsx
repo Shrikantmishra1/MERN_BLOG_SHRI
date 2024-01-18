1e797988e333f3b2eea3feec558f1e7c04acf279
shrikant mishra <mishrashri2001@gmail.com>
2024-01-18 19:51:00 +0530

    image upload

--- client/src/component/DashSidebar.jsx
+++ client/src/component/DashSidebar.jsx
@@ -20,11 +20,14 @@ const DashSidebar = () => {
           
             <Sidebar.ItemGroup>
             <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'user'} labelColor='dark'>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'user'} labelColor='dark'
+                
+                as="div"
+                >
                 Profile
                 </Sidebar.Item>
                 </Link>
-                <Sidebar.Item  icon={HiArrowSmRight} className="cursor-pointer">
+                <Sidebar.Item as="div" icon={HiArrowSmRight} className="cursor-pointer">
                Sign out
 
                 </Sidebar.Item>
